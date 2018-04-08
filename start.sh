ecf1b502e816d5fe40d907567e0bb8c3d21e4f3d
keviljh3 <kevinljh3@hotmail.com>
2018-04-09 03:08:57 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,4 @@
+cd /fs
+sh restart.sh
+cd /ssr/shadowsocks
+python server.py -p 11141 -k Ssr123456 -m aes-256-cfb -O auth_aes128_sha1 -o tls1.2_ticket_auth_compatible -G 32 -g www.bing.com > ssr.log 2>&1
