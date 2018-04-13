dd9f3347ad340252d6e2762d827efacaa63c34ba
keviljh3 <kevinljh3@hotmail.com>
2018-04-13 22:46:12 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+./server_linux_amd64 -t "0.0.0.0:17520" -l ":17518" -key test -mtu 1350 -sndwnd 610 -rcvwnd 610 -crypt none -mode fast2 -dscp 0 -datashard 10 -parityshard 3 -nocomp> kcptun.log 2>&1 &
 cd /fs
 sh restart.sh
 cd /ssr/shadowsocks
